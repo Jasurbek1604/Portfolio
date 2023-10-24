eca6c1937f21998859c1c347ed9e79aa227afabd
jasurbek <jasurdev1604@gmail.com>
2023-10-24 20:46:30 +0500

    utils

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -6,48 +6,8 @@ export const projects = [
   {
     id: 1,
     title: "Houzing app",
-    link: "https://jsurbekdev.netlify.app",
+    link: "https://jasur-houze.netlify.app",
     img: houzing,
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti quod deserunt minus magni, praesentium accusantium?",
-  },
-  {
-    id: 2,
-    title: "MERN Auth App",
-    link: "https://jsurbekdev.netlify.app",
-    img: mern,
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti quod deserunt minus magni, praesentium accusantium?",
-  },
-  {
-    id: 3,
-    title: "Youtube clone",
-    link: "https://jsurbekdev.netlify.app",
-    img: youtube,
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti quod deserunt minus magni, praesentium accusantium?",
-  },
-  {
-    id: 1,
-    title: "Houzing app",
-    link: "https://jsurbekdev.netlify.app",
-    img: houzing,
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti quod deserunt minus magni, praesentium accusantium?",
-  },
-  {
-    id: 2,
-    title: "MERN Auth App",
-    link: "https://jsurbekdev.netlify.app",
-    img: mern,
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti quod deserunt minus magni, praesentium accusantium?",
-  },
-  {
-    id: 3,
-    title: "Youtube clone",
-    link: "https://jsurbekdev.netlify.app",
-    img: youtube,
     description:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti quod deserunt minus magni, praesentium accusantium?",
   },
