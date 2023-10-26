2416d9c0c433696d71230dde5aa7f7f4c98e954f
jasurbek <jasurdev1604@gmail.com>
2023-10-26 10:14:10 +0500

    new Prject

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,6 +1,7 @@
 import mern from "../assets/mern.jpeg";
 import houzing from "../assets/houzing.png";
 import youtube from "../assets/youtube.png";
+import chatgpt from "../assets/chatgpt.jpg";
 
 export const projects = [
   {
@@ -11,4 +12,13 @@ export const projects = [
     description:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti quod deserunt minus magni, praesentium accusantium?",
   },
+  {
+    id:2 ,
+    title: "Chat GPT",
+    link: "https://jasur-houze.netlify.app",
+    img: chatgpt,
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti quod deserunt minus magni, praesentium accusantium?",
+  },
+
 ];
