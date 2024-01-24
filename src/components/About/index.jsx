1fdaec78deb5e502bb3d24eaf0dafa8b2be1572c
Jasurbek <jasurdev1604@gmail.com>
2024-01-25 04:28:40 +0500

    About me

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,116 +1,125 @@
 import React from "react";
 import Title from "../Title";
-
+import { useLangContext } from "../../context/LangContext";
+import { Content } from "./style";
+import { useColorContext } from "../../context/ColorContext";
+import { UseModeContext } from "../../context/ModeContenxt";
+import { useNavigate } from "react-router-dom";
+import cv from "../../assets/resume.pdf";
 const About = () => {
+  const [data, language, setLanguage, main] = useLangContext();
+  const [color] = useColorContext();
+  const [mode] = UseModeContext();
+  const navigate = useNavigate();
+  const handleClick = () => {
+    const cvFile = cv;
+    const downloadLink = document.createElement("a");
+    downloadLink.href = cvFile;
+    downloadLink.download = "Jasuebek O'telbayev CV.pdf";
+    downloadLink.click();
+  };
   return (
     <div className="container" data-aos="fade-left">
       <div className="wrapper">
-        <Title>About me</Title>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ducimus
-          aliquam a maxime, iste provident voluptas vero et, expedita modi
-          deserunt beatae quaerat itaque libero commodi. Commodi sunt totam
-          neque inventore velit vitae rem voluptas natus nam maiores incidunt
-          pariatur eaque at dolor iste ex quos, consectetur dicta tempora porro
-          reprehenderit mollitia eveniet? Impedit rem delectus officiis. Soluta
-          veniam quo esse minima voluptates aliquam maxime nulla, similique
-          nobis nostrum, ducimus quibusdam possimus, temporibus sit aperiam
-          modi. Nulla voluptate repudiandae, ut non, qui optio quae hic
-          voluptates voluptatum eum corporis dignissimos explicabo reprehenderit
-          assumenda. Molestiae culpa atque corporis repudiandae obcaecati at
-          consequatur minima vero aut, cum odio non eum maiores dolorum numquam
-          necessitatibus deserunt quos iusto fugiat? Impedit sapiente inventore
-          officia vel ab nobis, perferendis quia deserunt vitae veritatis ipsa
-          facere hic fugit necessitatibus, voluptatum debitis blanditiis illo?
-          Earum velit sapiente, reprehenderit explicabo quibusdam excepturi cum,
-          accusantium sint beatae ducimus nemo libero enim? Nulla ea, nisi harum
-          omnis, voluptatum possimus delectus cumque distinctio explicabo
-          laudantium non eaque maxime fugit, incidunt expedita magni. Vitae quam
-          atque repudiandae ea. Neque doloremque cupiditate dolores nobis quis
-          delectus molestiae recusandae illum similique nesciunt amet, odio
-          inventore omnis ut minima nihil asperiores deleniti nulla magni quos
-          iure fuga a! Enim sed dolorum adipisci tempora aperiam, a consequatur
-          iusto voluptatem ullam, nesciunt modi velit! Officiis pariatur modi
-          cum consectetur. Maxime non, adipisci accusantium porro quas molestias
-          possimus cum ea eos sint quia ipsa, nesciunt, culpa explicabo in natus
-          pariatur odio maiores? Quisquam distinctio voluptatum nihil ex
-          architecto voluptatibus quaerat eius illo modi. Esse dolores ex, magni
-          sit ab illo eum fugit quo repellat voluptatem ipsum consequatur
-          blanditiis aperiam at perferendis fugiat modi. Placeat ipsam quo
-          labore earum quasi beatae laborum autem, explicabo necessitatibus
-          pariatur debitis iusto doloribus numquam, possimus, porro mollitia
-          vero deserunt aspernatur ut! Odio quibusdam alias quisquam, nihil
-          molestias, beatae illo enim quia labore, deserunt vel recusandae.
-          Neque impedit cum eos, voluptatem, sequi iste repellat culpa, voluptas
-          ad tempora assumenda ipsa. Deleniti molestiae sapiente facilis
-          reiciendis accusantium vel rerum soluta magni harum quasi, quas
-          voluptas atque in dolore aspernatur ex quod aperiam pariatur
-          voluptatem nihil minus obcaecati cupiditate culpa! Corporis eum quia
-          quidem excepturi nemo quae necessitatibus ipsum labore, tempora ullam
-          perspiciatis inventore sed facilis expedita blanditiis neque voluptate
-          non. Eveniet eum quisquam doloribus molestiae cupiditate voluptas esse
-          repellendus distinctio rerum. Quos, laudantium consectetur ut eum
-          voluptatibus quas aspernatur impedit vero sed eveniet nihil. Debitis
-          eius aspernatur magnam illo reprehenderit atque reiciendis corrupti!
-          Sed porro, autem quos accusamus officiis iure exercitationem
-          repellendus quae suscipit asperiores ipsam dolorem totam eos nisi
-          corrupti expedita magnam quis pariatur aut error quo recusandae itaque
-          ab deleniti. Unde at architecto cumque non aut ipsam illo aperiam
-          accusamus. Vitae consectetur possimus iste magnam temporibus sunt
-          culpa? Earum odio assumenda illo. Sunt in sequi, ipsa, ipsum omnis cum
-          voluptatibus consectetur aliquid reiciendis fugiat recusandae
-          reprehenderit ipsam. Fuga vel sunt expedita illum aliquid, mollitia
-          autem quod, asperiores tenetur repellendus eveniet ut distinctio?
-          Vero, eos debitis. Ratione sed necessitatibus id facilis a!
-          Doloremque, non! Lorem, ipsum dolor sit amet consectetur adipisicing
-          elit. Cupiditate dolorum fugit vel repudiandae mollitia, voluptas
-          alias sequi odio quasi! Harum nesciunt cum praesentium dolores
-          voluptas numquam temporibus vero nisi, sint iusto repudiandae
-          blanditiis. Labore sapiente dolore molestiae doloribus. Consequatur,
-          animi dignissimos. Saepe, ducimus fugit temporibus ratione
-          perspiciatis sunt iure eius dignissimos, ut asperiores eaque
-          inventore, hic consectetur quod expedita! Commodi repellendus
-          perferendis repudiandae similique. Possimus neque ut magni, culpa nam
-          harum? Inventore, illo. Velit dolore autem commodi magni vel voluptate
-          fuga vitae iusto modi sed sit dolorem id optio architecto veritatis,
-          molestias tempora minus officiis distinctio sint obcaecati earum!
-          Perferendis eius magni repellendus reiciendis. Illo, cumque aperiam,
-          unde architecto adipisci, iste provident tempore necessitatibus nihil
-          dolores minima! Inventore ratione distinctio excepturi natus fuga
-          voluptatem aspernatur corrupti explicabo vel unde accusamus placeat
-          possimus perspiciatis, tempore asperiores quae voluptates
-          voluptatibus, in error dolore? Nam et optio numquam dolores delectus
-          ea, aperiam aliquid accusantium temporibus architecto fugit voluptas
-          dignissimos facilis voluptatem, mollitia dolorem ab adipisci obcaecati
-          labore odio vitae nostrum! Magni asperiores temporibus, explicabo
-          dolorum ex cum corporis accusantium sit soluta, nam, nostrum illum!
-          Sit dolore consectetur facere obcaecati, laudantium id. Dolore
-          necessitatibus veritatis aspernatur cupiditate minus voluptatibus!
-          Quis suscipit id ea distinctio esse corrupti illo sequi accusantium
-          corporis ab magni, consequatur eveniet enim labore obcaecati ratione
-          recusandae voluptates delectus tempora architecto et adipisci
-          excepturi? Id laudantium voluptatibus accusantium eius necessitatibus
-          natus eum at cumque possimus pariatur repellat ducimus consectetur
-          quod consequuntur debitis, velit assumenda iure cupiditate,
-          repellendus expedita voluptatum! Sint fugiat consequuntur voluptas,
-          expedita culpa explicabo ea blanditiis cupiditate! Cupiditate minus
-          dicta perferendis odit. Sed similique fugit possimus aspernatur iure
-          voluptate libero voluptatem nihil rem. Maiores ipsam cum dolorum
-          officia non, temporibus est, quos, fuga veniam vel itaque deleniti
-          sequi velit rem voluptates aspernatur aliquam? At doloremque laborum
-          doloribus eveniet repellendus odit sapiente, reprehenderit cumque
-          magni quibusdam tempore eligendi expedita iusto blanditiis maiores
-          iste facilis facere nesciunt architecto, id natus nulla saepe quo
-          totam! Quo praesentium maxime accusamus quam esse perspiciatis
-          repellat cum ipsum sequi aliquam, veniam ipsam nostrum non rerum
-          doloremque sed dicta ipsa ea alias odit natus expedita quos illo!
-          Corrupti soluta sed doloribus ad facilis recusandae? Odit fugiat rem
-          sit magni nisi velit ipsum provident quidem earum saepe enim, ex aut
-          nesciunt facilis consequatur blanditiis natus quod corporis,
-          laudantium beatae suscipit. Veritatis enim perspiciatis accusantium
-          fugiat, autem necessitatibus at cum libero nesciunt incidunt esse
-          magni deserunt iure aliquid itaque.
-        </p>
+        <Content color={color} mode={mode}>
+          <Title>About me</Title>
+          <div className="name">
+            {main?.name} <span>{main?.job}</span>
+          </div>
+          <div className="desc">
+            Junior Plus Web Developer specializing in front-end development.
+            Experienced with all stages of development cycle for dynamic web
+            projects. Well-versed in JavaScript and ReactjS. Strong background
+            in management and leadership.
+          </div>
+          <div className="grid">
+            <div className="grid__item">
+              <div className="grid__item__left">Age:</div>
+              <div className="grid__item__right">19</div>
+            </div>
+            <div className="grid__item">
+              <div className="grid__item__left">Country:</div>
+              <div className="grid__item__right">Tashkent</div>
+            </div>
+            <div className="grid__item">
+              <div className="grid__item__left">Education:</div>
+              <div className="grid__item__right">higher</div>
+            </div>
+            <div className="grid__item">
+              <div className="grid__item__left">Experience:</div>
+              <div className="grid__item__right">1 year</div>
+            </div>
+          </div>
+          <div className="buttons">
+            <button onClick={handleClick}>Download CV</button>
+            <button onClick={() => navigate("/contact")}>Hire Me</button>
+          </div>
+          <div className="techs">
+            <div className="techs__item">
+              <div className="techs__item__title">
+                <div>HTML</div>
+                <div>95%</div>
+              </div>
+              <div className="techs__item__content">
+                <div className="techs__item__content__tech html"></div>
+              </div>
+            </div>
+            <div className="techs__item">
+              <div className="techs__item__title">
+                <div>CSS, SCSS, SASS</div>
+                <div>95%</div>
+              </div>
+              <div className="techs__item__content">
+                <div className="techs__item__content__tech css"></div>
+              </div>
+            </div>
+            <div className="techs__item">
+              <div className="techs__item__title">
+                <div>JavaScript</div>
+                <div>90%</div>
+              </div>
+              <div className="techs__item__content">
+                <div className="techs__item__content__tech js"></div>
+              </div>
+            </div>
+            <div className="techs__item">
+              <div className="techs__item__title">
+                <div>React</div>
+                <div>90%</div>
+              </div>
+              <div className="techs__item__content">
+                <div className="techs__item__content__tech react"></div>
+              </div>
+            </div>
+            <div className="techs__item">
+              <div className="techs__item__title">
+                <div>TypeScript</div>
+                <div>75%</div>
+              </div>
+              <div className="techs__item__content">
+                <div className="techs__item__content__tech ts"></div>
+              </div>
+            </div>
+            <div className="techs__item">
+              <div className="techs__item__title">
+                <div>Ant Design, Material UI</div>
+                <div>50%</div>
+              </div>
+              <div className="techs__item__content">
+                <div className="techs__item__content__tech lib"></div>
+              </div>
+            </div>
+            <div className="techs__item">
+              <div className="techs__item__title">
+                <div>Tailwind CSS, Bootstrap</div>
+                <div>50%</div>
+              </div>
+              <div className="techs__item__content">
+                <div className="techs__item__content__tech lib"></div>
+              </div>
+            </div>
+          </div>
+        </Content>
       </div>
     </div>
   );
