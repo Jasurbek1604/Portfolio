08f5dc95b08d11fbd758e5791a3b343ae353ee63
Jasurbek <jasurdev1604@gmail.com>
2024-01-25 05:02:58 +0500

    About page mltiple languages

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -6,10 +6,12 @@ import { useColorContext } from "../../context/ColorContext";
 import { UseModeContext } from "../../context/ModeContenxt";
 import { useNavigate } from "react-router-dom";
 import cv from "../../assets/resume.pdf";
+import { useAboutContext } from "../../context/AboutContext";
 const About = () => {
   const [data, language, setLanguage, main] = useLangContext();
   const [color] = useColorContext();
   const [mode] = UseModeContext();
+  const [aboutData] = useAboutContext();
   const navigate = useNavigate();
   const handleClick = () => {
     const cvFile = cv;
@@ -22,37 +24,24 @@ const About = () => {
     <div className="container" data-aos="fade-left">
       <div className="wrapper">
         <Content color={color} mode={mode}>
-          <Title>About me</Title>
+          <Title>{aboutData?.title}</Title>
           <div className="name">
             {main?.name} <span>{main?.job}</span>
           </div>
-          <div className="desc">
-            Junior Plus Web Developer specializing in front-end development.
-            Experienced with all stages of development cycle for dynamic web
-            projects. Well-versed in JavaScript and ReactjS. Strong background
-            in management and leadership.
-          </div>
+          <div className="desc">{aboutData?.desc}</div>
           <div className="grid">
-            <div className="grid__item">
-              <div className="grid__item__left">Age:</div>
-              <div className="grid__item__right">19</div>
-            </div>
-            <div className="grid__item">
-              <div className="grid__item__left">Country:</div>
-              <div className="grid__item__right">Tashkent</div>
-            </div>
-            <div className="grid__item">
-              <div className="grid__item__left">Education:</div>
-              <div className="grid__item__right">higher</div>
-            </div>
-            <div className="grid__item">
-              <div className="grid__item__left">Experience:</div>
-              <div className="grid__item__right">1 year</div>
-            </div>
+            {aboutData?.data.map(({ id, left, right }) => (
+              <div className="grid__item" key={id}>
+                <div className="grid__item__left">{left}:</div>
+                <div className="grid__item__right">{right}</div>
+              </div>
+            ))}
           </div>
           <div className="buttons">
-            <button onClick={handleClick}>Download CV</button>
-            <button onClick={() => navigate("/contact")}>Hire Me</button>
+            <button onClick={handleClick}>{aboutData?.btn1}</button>
+            <button onClick={() => navigate("/contact")}>
+              {aboutData?.btn2}
+            </button>
           </div>
           <div className="techs">
             <div className="techs__item">
--- src/context/index.jsx
+++ src/context/index.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ModeProvider from "./ModeContenxt";
 import LangProvider from "./LangContext";
 import ColorProvider from "./ColorContext";
+import AboutProvider from "./AboutContext";
 
 const Context = ({ children }) => {
   return (
     <ModeProvider>
       <LangProvider>
-        <ColorProvider>{children}</ColorProvider>
+        <ColorProvider>
+          <AboutProvider>{children}</AboutProvider>
+        </ColorProvider>
       </LangProvider>
     </ModeProvider>
   );
